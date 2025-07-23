583cc5387f0015a50ee5f616dca5fb03ce7f8ff8
LuSiEDa <wltlrtmdgus@naver.com>
2025-07-23 18:48:20 +0900

    Update and rename READ.sql to DB/WORK/READ.sql

---
+++ DB/WORK/READ.sql
@@ -0,0 +1,15 @@
+USE practice01;
+
+SELECT name, salary FROM practice01.employees;
+
+-- Frontend 직챙 중 연봉 90000 이하
+
+SELECT name, salary FROM employees 
+WHERE position = 'Frontend' and salary <=90000;
+
+SELECT * FROM employees WHERE position = 'PM'
+
+-- 모든 직원의 포지션 별 평균 연
+
+SELECT position, AVG(salary) FROM employees
+GROUP BY position; 
